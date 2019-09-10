e1abb0da99e626d424f192d7cdac70cb46465b8e
k2interactive <k2interactivestudios@gmail.com>
2019-09-09 23:52:01 -0700

    chore: cleanup EnergyService

--- app/src/main/java/com/gemini/energy/service/EnergyService.kt
+++ app/src/main/java/com/gemini/energy/service/EnergyService.kt
@@ -2,7 +2,6 @@
 package com.gemini.energy.service
 
 import android.content.Context
-import android.util.Log
 import com.crashlytics.android.Crashlytics
 import com.gemini.energy.WordDocumentGenerator
 import com.gemini.energy.domain.Schedulers
@@ -109,20 +108,12 @@ class EnergyService(
     /**
      * Holds the Main Work - i.e Running each Energy Calculations for each of the IComputables
      * */
-//    var allValues = mutableListOf<EBase>()
     private fun doWork(callback: (status: Boolean) -> Unit) {
         Timber.d("####### DO WORK - COUNT [${taskHolder.count()}] - (${thread()}) #######")
-
-//        val valuesForDocumentGeneration = mutableListOf<Observable<IComputable>>()
 
         disposables.add(taskHolder.merge()
                 .observeOn(schedulers.observeOn)
                 .subscribe({
-
-                    //                    if (it.auditScopeType != null) {
-//                        valuesForDocumentGeneration.add(getIComputatble(it))
-//                    }
-
                 }, { exception ->
                     exception.printStackTrace()
                     Crashlytics.logException(exception)
@@ -131,41 +122,6 @@ class EnergyService(
                     Timber.d("**** Merge - [ON COMPLETE] ****")
 
                     WordDocumentGenerator().triggerGeneration(ebases)
-
-//                    disposables.add(valuesForDocumentGeneration.merge()
-//                            .subscribe ({
-//                                Log.i("-----moo", "wat is it?: ${it.javaClass.name}")
-//                                synchronized(ebaseValues) {
-//                                    val values = ebaseValues
-//
-//                                    val value = it as EBase
-//                                    value.compute {  }
-//
-//                                    allValues.add(value)
-//
-//                                    if (ebaseValues.any
-//                                            { obj ->
-//                                                obj.computable.auditScopeId == value.computable.auditScopeId
-//                                                        &&
-//                                                        obj.computable.auditId == value.computable.auditId
-//                                            }) {
-//                                    }
-//                                    else {
-//                                        ebaseValues.add(value)
-//                                    }
-//                                }
-//                            }, {
-//                                Log.i("-----moo", "EXCEPTION: ${it.localizedMessage}")
-//                            }, {
-//                                val maybes = ebaseValues
-//
-//                                WordDocumentGenerator().triggerGeneration(maybes)
-//
-//
-//                                Log.i("-----moo", "DONE")
-//                            }))
-
-
 
                     callback(true) // << ** The final Exit Point ** >> //
                 }))
@@ -180,8 +136,6 @@ class EnergyService(
      * */
     private fun getComputable(computable: Computable<*>): Observable<Observable<Computable<*>>> {
 
-//        var value
-
         fun build(computable: Computable<*>, featureAuditScope: List<Feature>,
                           featurePreAudit: List<Feature>): Observable<Computable<*>>{
 
@@ -193,65 +147,23 @@ class EnergyService(
                     utilityRateElectricity, usageHours,
                     outgoingRows, context).build()
 
-//            try {
-//                var value = returnable as EBase
-//                ebases.add(value)
-//            }
-//            catch(e: Exception) {
-//                print("EXCEPTION")
-//            }
-
             iComputable.compute().blockingLast()
 
-            // try next
             if (iComputable is EBase) {
                 ebases.add(iComputable)
-            } else {
-                Log.i("-----moo", "not ebase")
             }
 
-//            if (returnable.javaClass is EBase) {
-//                ebases.add(returnable as EBase)
-//            } else {
-//                Log.i("-----moo", "not ebase")
-//            }
-
             return iComputable.compute()
-//            return ComputableFactory.createFactory(computable, energyUtilityGas,
-//                    utilityRateElectricity, usageHours,
-//                    outgoingRows, context).build().compute()
         }
 
-//        Observable.zi
 
         return Observable.zip(
                 auditGateway.getFeature(computable.auditId),
                 auditGateway.getFeatureByType(computable.auditScopeId),
                 BiFunction<List<Feature>, List<Feature>, Observable<Computable<*>>> { featurePreAudit, featureAuditScope ->
-                    //                    Log.i("","")
                     build(computable, featureAuditScope, featurePreAudit)
                 })
     }
-
-//    private fun getIComputatble(computable: Computable<*>): Observable<IComputable> {
-//        fun build(computable: Computable<*>, featureAuditScope: List<Feature>, featurePreAudit: List<Feature>): IComputable {
-//            computable.featureAuditScope = featureAuditScope
-//            computable.featurePreAudit = featurePreAudit
-//
-//            val returnable = ComputableFactory
-//                    .createFactory(computable, energyUtilityGas, utilityRateElectricity, usageHours, outgoingRows, context)
-//                    .build()
-//            returnable.compute()
-//            return returnable
-//        }
-//
-//        return Observable.zip(
-//                auditGateway.getFeature(computable.auditId),
-//                auditGateway.getFeatureByType(computable.auditScopeId),
-//                BiFunction<List<Feature>, List<Feature>, IComputable> { featurePreAudit, featureAuditScope ->
-//                    build(computable, featureAuditScope, featurePreAudit)
-//                })
-//    }
 
     private fun thread() = Thread.currentThread().name
 
