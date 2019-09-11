b2d589cc1a1b54341a0c8703f95084b6213d54c5
akinslow2 <31294213+akinslow2@users.noreply.github.com>
2019-09-11 13:29:43 -0700

    Update Halogen.kt

--- app/src/main/java/com/gemini/energy/service/device/lighting/Halogen.kt
+++ app/src/main/java/com/gemini/energy/service/device/lighting/Halogen.kt
@@ -98,7 +98,7 @@ class Halogen (private val computable: Computable<*>, utilityRateGas: UtilityRat
         // Percentage Power Reduced - we get it from the Base - ELighting
 
         val energySavings = energyAtPreState * percentPowerReduced
-        val coolingSavings = energySavings * cooling * seer
+        val coolingSavings = energySavings * cooling / seer
 
         val energyAtPostState = energyAtPreState - energySavings
         val paybackmonth = selfinstallcost / energySavings * 12
