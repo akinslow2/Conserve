2ea171def08440b1a7913fd946e90b5c76ca29f7
akinslow2 <31294213+akinslow2@users.noreply.github.com>
2019-10-20 14:04:35 -0700

    Update Hvac.kt

--- app/src/main/java/com/gemini/energy/service/device/Hvac.kt
+++ app/src/main/java/com/gemini/energy/service/device/Hvac.kt
@@ -350,7 +350,7 @@ class Hvac(computable: Computable<*>, utilityRateGas: UtilityRate, utilityRateEl
         val delta = (powerPre - powerPost) * usageHours * quantity
 
         Timber.d("HVAC :: Delta -- $delta")
-  
+
         return delta
     }
 
