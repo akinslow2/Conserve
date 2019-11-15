ea394bcc011792034f696051542c314f02013632
akinslow2 <31294213+akinslow2@users.noreply.github.com>
2019-11-15 10:44:25 -0800

    Update UsageLighting.kt

--- app/src/main/java/com/gemini/energy/service/type/UsageLighting.kt
+++ app/src/main/java/com/gemini/energy/service/type/UsageLighting.kt
@@ -5,6 +5,9 @@ class UsageLighting : UsageHours() {
     var peakHours = 0.0
     var partPeakHours = 0.0
     var offPeakHours = 0.0
+    var postpeakHours = 0.0
+    var postpartPeakHours = 0.0
+    var postoffPeakHours = 0.0
 
     override fun timeOfUse(): TOU {
         return TOU(peakHours, partPeakHours, offPeakHours)
@@ -16,5 +19,5 @@ class UsageLighting : UsageHours() {
 
     //@Anthony - Verify if the Yearly hours is the sum as belows - regardless of TOU on No TOU
     override fun yearly() = peakHours + partPeakHours + offPeakHours
-
+    override fun postyearly() = postpeakHours + postpartPeakHours + postoffPeakHours
 }
