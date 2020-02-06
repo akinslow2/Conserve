00325ea56aef74432674145a73f0637e19a18c3f
k2interactive <k2interactivestudios@gmail.com>
2020-02-05 19:54:23 -0800

    fix: parse server url

--- app/src/main/java/com/gemini/energy/service/ParseAPI.kt
+++ app/src/main/java/com/gemini/energy/service/ParseAPI.kt
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject
 import io.reactivex.Single
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -93,13 +92,12 @@ class ParseAPI {
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl("http://ec2-18-216-25-245.us-east-2.compute.amazonaws.com.compute.amazonaws.com:80/parse/")
+                    .baseUrl("http://ec2-18-216-25-245.us-east-2.compute.amazonaws.com:80/parse/")
                     .client(okHttpClient.build())
                     .build()
 
             return retrofit.create(ParseAPIService::class.java)
         }
-
     }
 }
 
