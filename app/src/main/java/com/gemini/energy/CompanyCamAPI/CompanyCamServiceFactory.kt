5653e434906b1898289e26e6f3b6a9f88eb5ba40
k2interactive <k2interactivestudios@gmail.com>
2020-07-23 02:31:19 -0700

    chore: cleanup from code review
    (cherry picked from commit 69c70e11f5fc82bc3073a1a910af87b1cb74c7ff)

--- app/src/main/java/com/gemini/energy/CompanyCamAPI/CompanyCamServiceFactory.kt
+++ app/src/main/java/com/gemini/energy/CompanyCamAPI/CompanyCamServiceFactory.kt
@@ -5,31 +5,27 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.provider.ContactsContract
-import android.util.Log
 import com.gemini.energy.App
-import com.gemini.energy.branch
 import kotlinx.coroutines.*
-import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.net.URI
 
 
 object CompanyCamServiceFactory {
 
-    private const val API_BASE_URL = "https://app.companycam.com/"
-    private const val ACCESS_TOKEN = "e12b134258ccd3dff694581132e6192f998c54bddc1b5c5ea6f1363bd72c9b68"
-    private const val ROOT_USER_EMAIL = "akinslow2@geminiesolutions.com"
+    private const val apiBaseUrl = "https://app.companycam.com/"
+    // TODO:? get rid of me when possible
+    private const val accessToken = "e12b134258ccd3dff694581132e6192f998c54bddc1b5c5ea6f1363bd72c9b68"
+    private const val rootUserEmail = "akinslow2@geminiesolutions.com"
 
-    private const val CLIENT_ID = "8418eeee9e98af39e69160ba9e4127d55dcbd0e3f88a381e060c0ed571ca3e73"
-    private const val SECRET_KEY = "c1a14dd4c9099eed46c03f2b0394519412677a42a940d00605a1b65ceffe5722"
-    private const val REDIRECT_URI = "https://www.geminiesolutions.com/companycamauth"
+    private const val clientId = "8418eeee9e98af39e69160ba9e4127d55dcbd0e3f88a381e060c0ed571ca3e73"
+    private const val secretKey = "c1a14dd4c9099eed46c03f2b0394519412677a42a940d00605a1b65ceffe5722"
+    private const val redirectUri = "https://www.geminiesolutions.com/companycamauth"
 
-    private const val TOKEN_STORAGE = "companycamBearerToken"
+    private const val tokenPreferenceName = "companycamBearerToken"
 
     fun setToken(data: Uri) {
         val code = data.getQueryParameter("code") ?: return
@@ -46,23 +42,23 @@ object CompanyCamServiceFactory {
         val coroutineScope = CoroutineScope(mainActivityJob + Dispatchers.Main)
         coroutineScope.launch(errorHandler) {
 
-            val token = makeService().getAuthToken(Auth2RequestParameters(CLIENT_ID, SECRET_KEY, code, REDIRECT_URI))
+            val token = makeService().getAuthToken(Auth2RequestParameters(clientId, secretKey, code, redirectUri))
             setToken(token.access_token)
         }
     }
 
     fun setToken(authToken: String) {
-        val prefs: SharedPreferences = App.instance.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE)
+        val prefs: SharedPreferences = App.instance.getSharedPreferences(tokenPreferenceName, Context.MODE_PRIVATE)
         prefs.edit().putString("bearer-token", authToken).apply()
     }
 
     fun bearerToken(): String? {
-        val prefs: SharedPreferences = App.instance.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE)
+        val prefs: SharedPreferences = App.instance.getSharedPreferences(tokenPreferenceName, Context.MODE_PRIVATE)
         return prefs.getString("bearer-token", null)
     }
 
     fun clearAuthToken() {
-        val prefs: SharedPreferences = App.instance.getSharedPreferences(TOKEN_STORAGE, Context.MODE_PRIVATE)
+        val prefs: SharedPreferences = App.instance.getSharedPreferences(tokenPreferenceName, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = prefs.edit()
         editor.remove("bearer-token")
         editor.apply()
@@ -72,8 +68,8 @@ object CompanyCamServiceFactory {
     fun authorizeIntent(): Intent? {
         val authorizeUrl = "https://app.companycam.com/oauth/authorize?".toHttpUrlOrNull()
                 ?.newBuilder()
-                ?.addQueryParameter("client_id", CLIENT_ID)
-                ?.addQueryParameter("redirect_uri", REDIRECT_URI)
+                ?.addQueryParameter("client_id", clientId)
+                ?.addQueryParameter("redirect_uri", redirectUri)
                 ?.addQueryParameter("response_type", "code")
                 // TODO: get correct scope for read/write token
 //                ?.addQueryParameter("scope", "read+write+destroy")
@@ -100,14 +96,14 @@ object CompanyCamServiceFactory {
             if (token != null) {
                 newRequest.addHeader("Authorization", "Bearer $token")
                         // TODO: when multiple users, query and save user's email
-                    .addHeader("X-CompanyCam-User", ROOT_USER_EMAIL)
+                    .addHeader("X-CompanyCam-User", rootUserEmail)
             }
 
             chain.proceed(newRequest.build())
         }
 
         val builder = Retrofit.Builder()
-                .baseUrl(API_BASE_URL)
+                .baseUrl(apiBaseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
 
         return builder
