3487b5d28a24674ad0157c35630915ab03608e95
k2interactive <k2interactivestudios@gmail.com>
2020-07-25 18:43:33 -0700

    feat: update project address in company cam if address has changed since last upload to project
    (cherry picked from commit 3cf5dd3eacb255e555aa0ec3ba4a530100519804)

--- app/src/main/java/com/gemini/energy/CompanyCamAPI/CompanyCamService.kt
+++ app/src/main/java/com/gemini/energy/CompanyCamAPI/CompanyCamService.kt
@@ -1,34 +1,30 @@
 package CompanyCamAPI
 
-import CompanyCamAPI.Objects.Comment
 import CompanyCamAPI.Objects.Photo
 import CompanyCamAPI.Objects.Project
 import CompanyCamAPI.Objects.Tag
-import CompanyCamAPI.Requests.AddCommentRequest
 import CompanyCamAPI.Requests.Auth2RequestParameters
 import CompanyCamAPI.Requests.CreateProjectRequest
 import CompanyCamAPI.Requests.UploadPhotoRequest
 import CompanyCamAPI.Responses.Auth2Response
 import com.gemini.energy.CompanyCamAPI.Requests.ApplyTagToPhotoRequest
 import com.gemini.energy.CompanyCamAPI.Requests.CreateTagRequest
-import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import com.gemini.energy.CompanyCamAPI.Requests.UpdateProjectAddressRequest
+import retrofit2.http.*
 
 interface CompanyCamService {
-    // Authentication: step 1
-//    redirect the user to the CompanyCam authorization URI.
-    /// https://app.companycam.com/oauth/authorize
-    //      ?client_id={client_id}
-    //      &redirect_uri={authorized_redirect_uri}
-    //      &response_type=code
-    //      &scope=read+write+destroy
-
+    /* Authentication: step 1
+     * redirect the user to the CompanyCam authorization URI.
+      - https://app.companycam.com/oauth/authorize
+          ?client_id={client_id}
+          &redirect_uri={authorized_redirect_uri}
+          &response_type=code
+          &scope=read+write+destroy
+    */
     // Authentication: step 2
     @POST("oauth/token")
     suspend fun getAuthToken(@Body request: Auth2RequestParameters): Auth2Response
+
 
     // upload a photo
     @POST("v2/projects/{project_id}/photos/")
@@ -36,21 +32,35 @@ interface CompanyCamService {
             @Path("project_id") projectId: String,
             @Body uploadPhotoRequest: UploadPhotoRequest): Photo
 
+
     // creates a new project
     // returns the uploaded project object
     @POST("v2/projects/")
     suspend fun createProject(@Body createProjectRequest: CreateProjectRequest): Project
+
 
     // list all projects
     // Returns a list of projects you’ve previously created. The projects are returned in sorted order, with the most recent projects appearing first
     @GET("v2/projects?status=active")
     suspend fun getExistingProjects(): List<Project>
 
+
+    // updates address of the project
+    // returns the updated project
+    @PUT("v2/projects/{projects_id}")
+    suspend fun updateProjectAddress(
+            @Path("projects_id") projectId: String,
+            @Body address: UpdateProjectAddressRequest): Project
+
+
     // creates a new tag
     @POST("v2/tags/")
     suspend fun createTag(@Body content: CreateTagRequest): Tag
 
+
     // tags a photo with an existing tag
     @POST("v2/photos/{photo_id}/tags/")
-    suspend fun addTagToPhoto(@Path("photo_id") photoId: String, @Body tags: ApplyTagToPhotoRequest): List<Tag>
+    suspend fun addTagToPhoto(
+            @Path("photo_id") photoId: String,
+            @Body tags: ApplyTagToPhotoRequest): List<Tag>
 }
